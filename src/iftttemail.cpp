0f0954ce7240464666c3baff8e3a1170572d80d8
smart-life-tech <christlightworld@gmail.com>
2024-01-31 20:25:34 +0100

    modified:   src/iftttemail.cpp

--- src/iftttemail.cpp
+++ src/iftttemail.cpp
@@ -25,8 +25,24 @@ void setup()
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
-    Serial.println("After 10 seconds the first set of readings will be displayed");// you can edit this line to suit your message needs
+    Serial.println("After 10 seconds the first set of readings will be displayed"); // you can edit this line to suit your message needs
+    HTTPClient http;
+    WiFiClient client;
+    // http.begin(server_name);
+    http.begin(client, server_name);
 
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    // Data to send with HTTP POST
+    // String httpRequestData = "value1=" + String(random(50)) + "&value2=" + String(random(50)) + "&value3=" + String(random(50));
+    // Send HTTP POST request
+    String httpRequestData = "the button has just been pressed !";
+    int httpResponseCode = http.POST(httpRequestData);
+
+    Serial.print("HTTP Response code is: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
     // initialize a pseudorandom number
     randomSeed(analogRead(23));
 }
@@ -36,6 +52,7 @@ void loop()
     // Send an HTTP POST request every 10 seconds
     if ((digitalRead(buttonPin) == LOW))
     {
+        Serial.println("button has been pressed");
         // Check WiFi connection status
         if (WiFi.status() == WL_CONNECTED)
         {
